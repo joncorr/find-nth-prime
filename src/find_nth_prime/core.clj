716cfc0e1c4c20f03ff59f4940427c79b71b33cb
Jonathan <jonathan.e.corrales@gmail.com>
2017-08-12 07:28:06 -0400

    Added the prime? and get-nth-prime functions.

--- src/find_nth_prime/core.clj
+++ src/find_nth_prime/core.clj
@@ -1,7 +1,26 @@
 (ns find-nth-prime.core
   (:gen-class))
 
+(defn prime? [number]
+   (if (<= number 1)
+    false
+    (if (= number 2)
+     true
+     (= nil (first
+      (for [i (range 2 number)
+      :when (zero? (mod number i))] i))))))
+
+(defn get-nth-prime [n]
+ (loop [number 2 index 1]
+  (if (and (prime? number)(= n index))
+    number
+    (let [nextNumber (inc number)
+          isPrime (prime? number)
+          newIndex (if isPrime (inc index) index)]          
+         (recur nextNumber newIndex)
+        )
+    )))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (str "The 10,001st prime is " (get-nth-prime 10001))))
--- test/find_nth_prime/core_test.clj
+++ test/find_nth_prime/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [find-nth-prime.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest get-nth-prime-6thIs13
+ (testing "The 6th prime is 13"
+   (is (= (get-nth-prime 6) 13))))
+
+(deftest get-nth-prime-1stIs2
+ (testing "The 6th prime is 13"
+   (is (= (get-nth-prime 1) 2))))
+
+;(run-tests 'find-nth-prime.core-test)
