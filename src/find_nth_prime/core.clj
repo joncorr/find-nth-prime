a06aefb1cb40a5695b348bd8c48692eba5d19330
Jonathan <jonathan.e.corrales@gmail.com>
2017-08-12 06:58:41 -0400

    added initial files

---
+++ src/find_nth_prime/core.clj
@@ -0,0 +1,7 @@
+(ns find-nth-prime.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/find_nth_prime/core_test.clj
@@ -0,0 +1,7 @@
+(ns find-nth-prime.core-test
+  (:require [clojure.test :refer :all]
+            [find-nth-prime.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
